8d1225eb8e88a5e25ab536d694a454ec30d37913
Frisly-eng <stffern4@upana.edu.gt>
2025-11-13 19:29:09 -0600

    Update script.js
    este es el bueno

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ if (contactForm) {
         const data = Object.fromEntries(formData);
         
         // cambiada
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbyP2ZgSuMLPKO3ZcxfRKNTrpQ2y8nN1l2g9B1ixeEFpqVSeBV0nVGrgDSjdztaWogOM/exec';
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbx7OYBcEdlKRUzTokYWTE2rIwiSuzQoe4NIBxkN0Gf_Nc_ttwlq94knjRz0od7_UYeA9w/exec';
         
         try {
             const response = await fetch(scriptURL, {
@@ -113,6 +113,7 @@ window.addEventListener('scroll', () => {
         navbar.style.boxShadow = '0 4px 6px -1px rgba(0, 0, 0, 0.1)';
     }
 });
+
 
 
 
