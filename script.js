bb6cc292c75386374c3328e314f41ae8ec7268d2
Frisly-eng <stffern4@upana.edu.gt>
2025-11-14 20:01:08 -0600

    Update script.jsAHHHH

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ if (contactForm) {
         const data = Object.fromEntries(formData);
         
         // cambiada
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbwl9QhtZU9dD4ABWP_wMq5f79AXa3KcNOHOA9j8DS8a16NzNFv2hnNTO4zUo-r-GiFO2A/exec';
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwkJMNvNyIZqclyUbzlMeo98gOrpnY09o7FmfXaCRtOOkqGWPWYCwOAeXt-fUmoirx8fw/exec';
         
         try {
             const response = await fetch(scriptURL, {
@@ -113,4 +113,5 @@ window.addEventListener('scroll', () => {
         navbar.style.boxShadow = '0 4px 6px -1px rgba(0, 0, 0, 0.1)';
     }
 });
+
 
